281ec3d80577b86b2468a0e7dad00470cea03e85
JiangNing <jinniudashu@gmail.com>
2020-10-21 13:57:45 +0800

    OK

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 var app = new Vue({
   el: "#chart",
   data: {
-    width: 200,
-    height: 200,
-    x: 10,
-    y: 10
+    chartWidth: 500,
+    chartHeight: 400,
+    chartData: [5, 8, 20, 3, 17, 14]
+    // width: 200,
+    // height: 200,
+    // x: 10,
+    // y: 10
+  },
+  methods: {
+    barWidth(value) {
+      return (this.chartWidth / this.dataMax) * value;
+    }
+  },
+  computed: {
+    dataMax() {
+      return Math.max(...this.chartData);
+    }
   }
 });
